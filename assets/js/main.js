6d28ee6b6f9f160cef837636762bc56a7487d46e
Lucas Delgado <golucasdelgado@icloud.com>
2026-02-11 12:22:11 -0800

    Moved RSVP section to the bottom and adjusted sectino spacing

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,7 +206,7 @@ END:VCALENDAR`;
 // ======================
 function initScrollFadeIn() {
   const observerOptions = {
-    threshold: 0.1,
+    threshold: 0,
     rootMargin: '0px 0px -50px 0px'
   };
   
